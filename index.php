4850c22b897448645461a82e3f674abedd7777e1
aod <teeraphong.won@ricoh.co.th>
2024-07-05 09:39:40 +0700

    lorem

--- index.php
+++ index.php
@@ -9,5 +9,6 @@
     <h1>Hello world</h1>
     <h3>Welcome Gitea</h3>
     <h3>Test tree</h3>
+    <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Repudiandae autem dolorem ex molestiae, explicabo odio esse sit amet sequi, aliquam ipsa quas blanditiis maiores voluptate, nostrum officiis temporibus quaerat quia!</p>
 </body>
 </html>
